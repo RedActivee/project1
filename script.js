47dab4e8f964c14f4d063dc27df5c63903df3699
Dmitry <nohopenolife@mail.ru>
2022-02-14 22:43:50 +0300

    What a functioin?

--- script.js
+++ script.js
@@ -22,16 +22,76 @@
 Проверить, чтобы все работало без ошибок в консоли */
 
 'use strict';
-let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
+
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?',undefined);
+
+    while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?',undefined);
+    }
+}
+start();
+
 let personalMovieDB = {
     count: numberOfFilms,
-    movies: {},
+    movies: {}, 
     actors: {},
     genres: [],
     privat: false
 }
 
-const a = prompt('Один из последних просмотренных фильмов?',""),
+function rememberMyFilms() {
+    for (let i = 0; i < 2;i++){
+    
+        const a = prompt('Один из последних просмотренных фильмов?',"");
+        if (a === "" || a.length === 50){
+            console.log('Недопустимо использовать пустую строку или же надпись более 50 символов');
+            i--;
+        }else{ const b = prompt('На сколько оцените его?',"");
+        personalMovieDB.movies[a] = b;}
+    }
+}
+rememberMyFilms();
+
+
+
+
+function detectPersonalLevel() {
+    if(numberOfFilms <= 10){
+        console.log('Маловато будет')
+    }else if (numberOfFilms === 10 || numberOfFilms <= 30){
+        console.log('Нормулёк')
+    }else if(numberOfFilms >= 30){
+        console.log('Вы вообще человек?')
+    }else if ( numberOfFilms !== Number ){
+        console.log('Ошибочка вышла')
+    }
+}
+detectPersonalLevel();
+
+function showMyDB(hiden) {
+    if(!hiden) {
+       console.log(personalMovieDB); 
+    }
+}
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+    for (let i = 0; i < 3;i++){
+    
+        const a = prompt(`Ваш любимый жанр под номером ${i+1}`,"");
+        if (a === "" || a.length === 50){
+            console.log('Недопустимо использовать пустую строку или же надпись более 50 символов');
+            i--;
+        };
+        personalMovieDB.genres.push(a);
+        }
+    }
+writeYourGenres();
+
+/* const a = prompt('Один из последних просмотренных фильмов?',""),
       b = prompt('На сколько оцените его?',""),
       c = prompt('Один из последних просмотренных фильмов?',""),
       d = prompt('На сколько оцените его?',"");
@@ -39,4 +99,4 @@ const a = prompt('Один из последних просмотренных ф
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+console.log(personalMovieDB); */
